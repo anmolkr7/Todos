86889b17bacc0f6e089705ad52797efe8cfbf08b
anmolkr7 <anmolojha952@gmail.com>
2025-02-15 16:38:57 +0530

    Components

---
+++ frontend/src/components/Createtodo.jsx
@@ -0,0 +1,9 @@
+export function CreateTodo(){
+    return(
+        <div>
+            <input type="text" placeholder="Enter Title" /> <br/>
+            <input type="text" placeholder="Enter Description"/> <br/>
+            <button>Add Todo</button>
+        </div>
+    )
+}
