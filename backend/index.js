68c383a266090ebdf4b8ef6a90aeb7cadc532c3e
anmolkr7 <anmolojha952@gmail.com>
2025-02-15 18:14:49 +0530

    FIXING CORS

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { createTodo, updateTodo } = require('./types');
 const { todo } = require('./database');
 const app = express();
+const cors=require("cors")
 const PORT = 3000;
 app.use(express.json());
-
+app.use(cors())
 /*
 body{
 title:string
